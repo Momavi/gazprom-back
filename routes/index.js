e6f82fda465a30fb43efde98e69b2db8e9922873
Erbis Vladislav <cool.ru1960@gmail.com>
2023-07-17 20:39:17 +0300

    [UPD] Быстрый баг фикс

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res,) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/files", function(req, res, next) {
+router.get("/files", function(req, res) {
   const files = [
     { id: 1, name: "File 1" },
     { id: 2, name: "File 2" },
